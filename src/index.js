47042bce11c4e3876ec6a79f74fda157e670b22d
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2021-11-08 23:54:39 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './sass/main.scss';
 
 // import './js/about.js';
 import './js/header.js';
+import './js/searchByKeyword'
 import './js/modal-film.js'
--- src/js/modal-film.js
+++ src/js/modal-film.js
@@ -1,33 +1,43 @@
 import modalFilm from '../partials/hbs/modal-film.hbs'
-// console.log(modalFilm);
-
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = '7cb7f2a84f35ebc2678afebafcd2cb5f';
-// https://api.themoviedb.org/3/movie/{movie_id}/keywords?api_key=<<api_key>>
-function fetchMovie(movie_id) {
-    return fetch(`${BASE_URL}/movie/${movie_id}/keywords?api_key=${API_KEY}`)
-    .then(response => console.log(response.json()))
-}
-
-export default { fetchMovie };
+import API from '../js/apiService'
 
 const containerRef = document.querySelector('.modal-film-container')
+const containerList = document.querySelector('.my-container')
 const modalWindow = document.querySelector('.modal-film')
 const backdrop = document.querySelector('.backdrop')
 const closeBtnModal = document.querySelector('[data-action="close-btn-modal"]')
+const image = document.getElementsByTagName('img')
 
 closeBtnModal.addEventListener('click', onCloseBtnModal)
 backdrop.addEventListener('click', onCloseBtnModal)
 window.addEventListener('keydown', onEcsKeyPress)
+containerList.addEventListener('click', onClickMovie)
+
+function onClickMovie(e) {
+  API.fetchMovie(e.target.id).then(results => {
+    console.log(e.target.id)
+    console.log(results.id)
+    if (e.target.id == results.id) {
+      containerRef.insertAdjacentHTML('beforeend', modalFilm(results))
+    }
+  })
+    
+  if (e.target.nodeName !== 'IMG') {
+    return
+  }
+  backdrop.classList.remove('visually-hidden')
+  containerRef.classList.add('is-open')
+  modalWindow.classList.add('is-open')
+}
 
 function onCloseBtnModal() {
     modalWindow.classList.remove('is-open')
     backdrop.classList.add('visually-hidden')
   window.removeEventListener('keydown', onEcsKeyPress)
+  containerRef.innerHTML = ''
 }
 
 function onEcsKeyPress(e) {
-    console.log(e.code);
   if (e.code !== 'Escape') {
     return
   }
