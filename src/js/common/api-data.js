dde22385019d207d35b891b31a4882ef71cddb78
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-11-09 18:12:26 +0200

    bugfix and refactoring

--- src/js/common/api-data.js
+++ src/js/common/api-data.js
@@ -1,32 +1,29 @@
 export function get(user) {
-  if (!user) {
-    return [];
-  }
-
   let data = getData();
+
   if (!data) {
     return [];
   } else {
-    if (!data[user]) {
-      return [];
+    if (!user) {
+      return data;
     } else {
-      return data[user];
+      if (!data[user]) {
+        return [];
+      } else {
+        return data[user];
+      }
     }
   }
 }
 
-export function put(user, data) {
+export function put(user, key, data) {
   if (!user) {
     return false;
   }
 
   let tmp = getData();
-  if (tmp) {
-    tmp[user] = data;
-  } else {
-    tmp = { [user]: data };
-  }
-  console.log('tmp', tmp);
+  tmp = checkUser(tmp, user);
+  tmp[user] = setKey(tmp[user], key, data);
   try {
     tmp = JSON.stringify(tmp);
     localStorage.setItem('themoviedb', tmp);
@@ -34,6 +31,37 @@ export function put(user, data) {
   } catch (error) {
     console.log(`Something happened: ${error}`);
     return false;
+  }
+}
+
+function checkUser(data, user) {
+  if (!data) {
+    data = { [user]: {} };
+  }
+  if (!data[user]) {
+    data[user] = {};
+  }
+  return data;
+}
+
+function setKey(data, key, value) {
+  if (!data[key] || !Array.isArray(data[key])) {
+    data[key] = [];
+  }
+
+  if (Array.isArray(value)) {
+    for (const item of value) {
+      addData(data[key], item);
+    }
+  } else {
+    addData(data[key], value);
+  }
+  return data;
+}
+
+function addData(data, value) {
+  if (data.indexOf(value) === -1) {
+    data.push(value);
   }
 }
 
