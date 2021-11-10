bdcc9e9e51d9ffcadc6f0aef2387587553629939
Vitaliy-Karpenko529 <vkarpenko945@gmail.com>
2021-11-10 21:38:55 +0200

    finisf marcup

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,11 +1,12 @@
-// var1
+
 let mask = document.querySelector('.mask');
 
 window.addEventListener('load', () => {
     mask.classList.add('hide');
+
     setTimeout(() => {
         mask.remove();
     }, 600);
 });
 
-//  var 3 ===============================
+
