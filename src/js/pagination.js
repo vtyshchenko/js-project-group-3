afa9a905b7b67f4d15ce38a0c075160a2298808b
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-11-12 19:01:40 +0200

    added button control functionality

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -16,59 +16,112 @@ import buttonsTpl from '../partials/hbs/pagination.hbs';
 
 // fetchPages();
 
-const wrapper = document.querySelector('.pagination-wrapper-js');
+const wrapper = document.querySelector('.pagination__wrapper-js');
 const arrowLeft = document.querySelector('.arrow-left-js');
 const arrowRight = document.querySelector('.arrow-right-js');
+const listernerEvent = document.querySelector('.pagination__wrapper-js');
 
 const firstPage = 1;
-const page = 14;
+let page = 1;
 const totalPages = 20;
 const countShowSumbols = 9;
 const MOVE_PAGE_TEXT = '...';
 
-function markupButton(totalPages, page) {
+arrowLeft.addEventListener('click', onClickArrowLeft);
+arrowRight.addEventListener('click', onClickArrowRight);
+listernerEvent.addEventListener('click', onClickButton);
+
+function onClickArrowLeft() {
+  page -= 1;
+  onMarkupButton(totalPages, page);
+  return page;
+}
+
+function onClickArrowRight() {
+  page += 1;
+  onMarkupButton(totalPages, page);
+  return page;
+}
+
+function onClickButton(e) {
+  let buttonText = e.target.innerText;
+  if (buttonText !== MOVE_PAGE_TEXT) {
+    page = Number(buttonText);
+  } else {
+    let button = e.target;
+    while (button.nodeName !== 'LI') {
+      button = button.parentNode;
+    }
+    let buttonArr = Array.from(button.classList);
+    if (buttonArr.includes('next')) {
+      page += 3;
+    } else {
+      page -= 3;
+    }
+    if (page < 0) {
+      page = 1;
+    } else if (page > totalPages) {
+      page = totalPages;
+    }
+  }
+  onMarkupButton(totalPages, page);
+}
+
+function onHideArrow() {
+  this.classList.add('hidden');
+}
+
+function onShowArrow() {
+  this.classList.remove('hidden');
+}
+
+function onMarkupButton(totalPages, page) {
   let beforePages;
   let afterPages;
+
   if (page > 0 && page < 6) {
     beforePages = firstPage;
   } else {
     beforePages = page - 4;
   }
 
-  if (page < 17) {
+  if (page < totalPages - 3) {
     afterPages = page + 4;
   } else {
     afterPages = totalPages;
-    beforePages = countShowSumbols + 3;
+    beforePages = totalPages - countShowSumbols + 1;
   }
 
   if (page === 1) {
-    //*Зробити активною стрілку вліво
+    //*Зробити не активною стрілку вліво
     arrowLeft.classList.add('hidden');
-  }
-  let buttons = buttonsTpl(1);
-  if (page > 5) {
-    buttons += buttonsTpl(MOVE_PAGE_TEXT);
   } else {
-    buttons += buttonsTpl(2);
+    arrowLeft.classList.remove('hidden');
+  }
+  let buttons = '';
+  //* якщо сторінка від 1 до 5 не для мобілки 108-114
+  buttons += buttonsTpl({ id: '', name: 1 });
+  if (page > 5) {
+    buttons += buttonsTpl({ id: 'prev', name: MOVE_PAGE_TEXT });
+  } else {
+    buttons += buttonsTpl({ id: '', name: 2 });
     afterPages = countShowSumbols;
   }
 
-  console.log('beforePages:', beforePages);
-  console.log('afterPages:', afterPages);
   for (let pageLength = beforePages + 2; pageLength <= afterPages - 2; pageLength++) {
-    buttons += buttonsTpl(pageLength);
+    buttons += buttonsTpl({ id: '', name: pageLength });
   }
+  //* не для мобілки 120-126
   if (totalPages - page > 4) {
-    buttons += buttonsTpl(MOVE_PAGE_TEXT);
+    buttons += buttonsTpl({ id: 'next', name: MOVE_PAGE_TEXT });
   } else {
-    buttons += buttonsTpl(totalPages - 1);
+    buttons += buttonsTpl({ id: '', name: totalPages - 1 });
   }
-  buttons += buttonsTpl(totalPages);
+
+  buttons += buttonsTpl({ id: '', name: totalPages });
   wrapper.innerHTML = buttons;
 
   const initialPage = document.querySelectorAll('.pagination__link-js');
-  console.log('🚀 ~ file: pagination.js ~ line 70 ~ markupButton ~ initialPage', initialPage);
   for (const button of initialPage) {
     if (button.innerText === String(page)) {
       //*Додати клас "pagination__link-current"
@@ -78,17 +131,10 @@ function markupButton(totalPages, page) {
   }
 
   if (page === totalPages) {
-    //*Зробити активною кнопку вправо
     arrowRight.classList.add('hidden');
+  } else {
+    arrowRight.classList.remove('hidden');
   }
-
-  // wrapper.innerHTML = buttonsTpl(page);
-
-  // for (let page = 1; page <= totalPages; page++) {
-  //   if (page > 1) {
-  //     arrowLeft.disabled = true;
-  //   }
-  // }
 }
 
-markupButton(totalPages, page);
+onMarkupButton(totalPages, page);
