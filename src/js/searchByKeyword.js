ed49eb15e8b20389e475c55f1609deb531b7e617
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-11-12 14:57:50 +0200

    bugfix

--- src/js/searchByKeyword.js
+++ src/js/searchByKeyword.js
@@ -1,53 +1,51 @@
 import API from './apiService';
 import movieCardTpl from '../partials/hbs/video-card.hbs';
-// import { onSearchYear } from './searchGenresAndYear.js';
-// import { onSearchGenresList } from './searchGenresAndYear.js';
+import { onSearchYear, onSearchGenresList } from './searchGenresAndYear.js';
 import refs from './common/refs';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/desktop/dist/PNotifyDesktop';
 import '@pnotify/core/dist/BrightTheme.css';
 import { notice, error } from '@pnotify/core';
 
-const { galleryListRefs, inputSearchRefs } = refs.refs
+const { galleryListRefs, inputSearchRefs } = refs.refs;
 
-const debounce = require('lodash.debounce')
-inputSearchRefs.addEventListener('input', debounce(onSearch, 500))
+const debounce = require('lodash.debounce');
+inputSearchRefs.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch() {
-    if (!inputSearchRefs.value) {
-        return notice({
-            text: 'Please enter your search query.',
-            delay: 3000,
-        })
-    } 
-    API.fetchMovies(inputSearchRefs.value.trim())
-        // .then(onSearchYear)
-        // .then(onSearchGenresList)
-        .then(movieStatus)
-        .then(results => {
-            onRenderMoviesCard(results);
-            localStorage.setItem("pageType", "search by keyword");
-            localStorage.setItem("totalPages", results.total_pages); 
-        })
-        .catch(onFetchError)
+  if (!inputSearchRefs.value) {
+    return notice({
+      text: 'Please enter your search query.',
+      delay: 3000,
+    });
+  }
+  API.fetchMovies(inputSearchRefs.value.trim())
+    .then(onSearchGenresList)
+    .then(onSearchYear)
+    .then(movieStatus)
+    .then(results => {
+      onRenderMoviesCard(results);
+      localStorage.setItem('pageType', 'search by keyword');
+      localStorage.setItem('totalPages', results.total_pages);
+    })
+    .catch(onFetchError);
 }
 
 function movieStatus(results) {
   if (results.total_results === 0) {
-    onRenderMoviesCard(movies)
+    onRenderMoviesCard(movies);
   }
-  return Promise.resolve(results)
+  return Promise.resolve(results);
 }
 
 function onRenderMoviesCard(movies) {
-    const markup = movieCardTpl(movies.results);
-    galleryListRefs.innerHTML = markup;
+  const markup = movieCardTpl(movies.results);
+  galleryListRefs.innerHTML = markup;
 }
 
 function onFetchError() {
-    return error({
-        text: 'Search result not successful. Enter the correct movie name!',
-        delay: 4000,
-    });
+  return error({
+    text: 'Search result not successful. Enter the correct movie name!',
+    delay: 4000,
+  });
 }
-
