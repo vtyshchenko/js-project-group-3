ba36e513a109fa0215646da0b75076ea1ec4d346
ViktoriiaPavlenko <vktr9999@ukr.net>
2021-11-15 12:55:20 +0200

    made corrections from mentor

--- src/js/watched-queue-btns.js
+++ src/js/watched-queue-btns.js
@@ -19,8 +19,7 @@ function getMarkup(name) {
         dataList = data.watched
         if (data.watched) {
             totalPages = numberOfPage(data.watched)
-        }
-        
+        }    
     } else {
         dataList = data.queue
         if (data.queue) {
@@ -28,6 +27,7 @@ function getMarkup(name) {
         }
     }
     localStorage.setItem('totalPages', totalPages);
+    localStorage.setItem('pageType', name);
     let genresList = onSearchGenresList(dataList)
     let year = onSearchYear(genresList)
     if (year) {
@@ -36,23 +36,17 @@ function getMarkup(name) {
         galleryListRefs.innerHTML = ''
         return notice({
             text: 'Oops! You have no movies here.',
-            delay: 3000,
+            delay: 2000,
         }) 
     }
 }
 
 export default function onWatchedBtnClick() {
     getMarkup('watched')
-    typeOfPage('watched')
 }
 
 function onQueueBtnClick() {
     getMarkup('queue')
-    typeOfPage('queue')
-}
-
-function typeOfPage(type) {
-    localStorage.setItem('pageType', type);
 }
 
 function numberOfPage(info) {
