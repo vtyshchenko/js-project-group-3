960d7bb977116eca125aa45a5214cac8c928e3a2
dimperesichan <sobaka.n.s.1@ukr.net>
2021-11-16 22:15:41 +0200

    button-doun-scroll

--- src/js/up-button.js
+++ src/js/up-button.js
@@ -1,23 +1,23 @@
 let goTopBtn = document.querySelector('.up-button');
 
+function trackScroll() {
+  let scrolled = window.pageYOffset;
+  let coords = document.documentElement.clientHeight;
+  
+  if (scrolled > coords) {
+    goTopBtn.classList.add('up-button-show');
+  }
+  if (scrolled < coords) {
+    goTopBtn.classList.remove('up-button-show');
+  }
+}
+
+function upButton() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -30);
+    setTimeout(upButton, 0);
+  }
+}
+
 window.addEventListener('scroll', trackScroll);
 goTopBtn.addEventListener('click', upButton);
-
-function trackScroll() {
-    let scrolled = window.pageYOffset;
-    let coords = document.documentElement.clientHeight;
-
-    if (scrolled > coords) {
-      goTopBtn.classList.add('up-button-show');
-    }
-    if (scrolled < coords) {
-      goTopBtn.classList.remove('up-button-show');
-    }
-  }
-
-  function upButton() {
-    if (window.pageYOffset > 0) {
-      window.scrollBy(0, -30);
-      setTimeout(upButton, 0);
-    }
-  }
