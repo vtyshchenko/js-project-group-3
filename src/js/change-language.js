1751036ece67b7a571efb06376be63b8f7fa42b4
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2021-11-17 23:06:09 +0200

    translate footer

--- src/js/change-language.js
+++ src/js/change-language.js
@@ -2,8 +2,12 @@ import { setLanguage } from './common/api-data'
 import refs from './common/refs'
 import { getLanguage } from './common/api-data'
 
+const footerRightsRef = document.querySelector('.footer-cont__txt-rights');
+const footerDeveloped = document.querySelector('.footer-cont__txt-developed');
+const byFooterRef = document.querySelector('.footer-cont__text-by');
+
 const{ ukrLangSvgRefs, engLangSvgRefs, btnHomeRefs, btnLibrRefs, inputSearchRefs, btnLogin, headerWatchedBtnRefs,
-  headerQueueBtnRefs, watchedBtnRefs, queueBtnRefs } = refs.refs
+  headerQueueBtnRefs, watchedBtnRefs, queueBtnRefs, openModalBtnRefs } = refs.refs
 
 ukrLangSvgRefs.addEventListener('click', onChangeLangUA)
 engLangSvgRefs.addEventListener('click', onChangeLangUS)
@@ -20,7 +24,7 @@ function onChangeLangUS() {
 
 function ukrLangSvgClick() {
   if (localStorage.getItem('language') === 'uk-UA') {
-    btnHomeRefs.innerHTML='Додому'
+    btnHomeRefs.innerHTML='Головна'
     btnLibrRefs.innerHTML ='Моя бібліотека'
     inputSearchRefs.placeholder = 'Шукати фільми'
     btnLogin.innerHTML = 'Увійти'
@@ -28,6 +32,10 @@ function ukrLangSvgClick() {
     headerQueueBtnRefs.innerHTML = 'У черзі'
     watchedBtnRefs.innerHTML = 'До переглянутих'
     queueBtnRefs.innerHTML = 'Дивитися потім';
+    footerRightsRef.innerHTML = 'Всі права захищені'
+    footerDeveloped.innerHTML = 'Розроблено з'
+    openModalBtnRefs.innerHTML = 'студентами GoIT'
+    byFooterRef.innerHTML = ''
     ukrLangSvgRefs.style.border = '1px solid #ff6b01'
   }
 }
