2e884c21286ef508bcd920bc79691f3de854abf8
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-11-17 23:10:47 +0200

    bugfixes

--- src/js/up-button.js
+++ src/js/up-button.js
@@ -1,7 +1,5 @@
 let goTopBtn = document.querySelector('.up-button');
 let btnScrollDown = document.querySelector('#scroll-down');
-console.log('btnScrollDown', btnScrollDown);
-console.log('document.documentElement', document.documentElement);
 
 function trackScroll() {
   let scrolled = window.pageYOffset;
@@ -17,12 +15,9 @@ function trackScroll() {
   if (coords - scrolled > 100) {
     btnScrollDown.classList.add('up-button-show');
   }
-  console.log('classes', btnScrollDown.classList);
   if (coords - scrolled < 100 && !Array.from(btnScrollDown.classList).includes('up-button-show')) {
     btnScrollDown.classList.remove('up-button-show');
   }
-  console.log('coords', coords, scrolled);
-  console.log('coords - scrolled', coords - scrolled);
 }
 
 function upButton() {
@@ -33,13 +28,16 @@ function upButton() {
 }
 
 function scrollDown() {
-  let coords = document.documentElement.scrollTop;
-  console.log(window.pageYOffset);
-  console.log('document.documentElement.clientHeight', coords);
-  if (window.pageYOffset < coords) {
-    window.scrollBy(0, 10);
-    setTimeout(scroll, 0);
-  }
+  var windowCoords = document.documentElement.clientHeight;
+  (function scroll() {
+    if (window.pageYOffset < windowCoords) {
+      window.scrollBy(0, 10);
+      setTimeout(scroll, 0);
+    }
+    if (window.pageYOffset > windowCoords) {
+      window.scrollTo(0, windowCoords);
+    }
+  })();
 }
 
 window.addEventListener('scroll', trackScroll);
