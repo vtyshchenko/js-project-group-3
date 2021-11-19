7cf765d686052422085165b9a1e5e309aef752a9
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-11-19 10:33:14 +0200

    bugfixes

--- src/js/up-button.js
+++ src/js/up-button.js
@@ -1,5 +1,4 @@
 let goTopBtn = document.querySelector('.up-button');
-let btnScrollDown = document.querySelector('#scroll-down');
 
 function trackScroll() {
   let scrolled = window.pageYOffset;
@@ -12,13 +11,6 @@ function trackScroll() {
   if (scrolled < 100) {
     goTopBtn.classList.remove('up-button-show');
   }
-
-  if (coords - scrolled > 100) {
-    btnScrollDown.classList.add('up-button-show');
-  }
-  if (coords - scrolled < 100 && !Array.from(btnScrollDown.classList).includes('up-button-show')) {
-    btnScrollDown.classList.remove('up-button-show');
-  }
 }
 
 function upButton() {
@@ -28,19 +20,5 @@ function upButton() {
   }
 }
 
-function scrollDown() {
-  let windowCoords = document.documentElement.scrollHeight;
-  (function scroll() {
-    if (window.pageYOffset < windowCoords) {
-      window.scrollBy(0, 10);
-      setTimeout(scrollDown, 0);
-    }
-    if (window.pageYOffset > windowCoords) {
-      window.scrollTo(0, windowCoords);
-    }
-  })();
-}
-
 window.addEventListener('scroll', trackScroll);
 goTopBtn.addEventListener('click', upButton);
-btnScrollDown.addEventListener('click', scrollDown);
